8e03c31bf720d9480c280e3d445f67aabe818f25
JokerDev <119523052+comermenudeldia@users.noreply.github.com>
2025-03-21 11:32:24 +0100

    up

--- src/app/admin/empresas/page.tsx
+++ src/app/admin/empresas/page.tsx
@@ -342,7 +342,7 @@ export default function EmpresasPage() {
               });
             },
           });
-        } catch (error: any) {
+        } catch (error: unknown) {
           console.error("Error al eliminar la cuenta:", error);
           setModalData({
             isOpen: true,
